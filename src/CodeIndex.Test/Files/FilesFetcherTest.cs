b387d0479289625c50837d379c204bba1b9bf3d3
qiuhao <qiu66hao@163.com>
2020-01-19 10:47:46 +0800

    UT

--- src/CodeIndex.Test/Files/FilesFetcherTest.cs
+++ src/CodeIndex.Test/Files/FilesFetcherTest.cs
@@ -17,11 +17,11 @@ namespace CodeIndex.Test
             File.Create(Path.Combine(TempDir, "SubDir", "ddd.xml")).Close();
             File.Create(Path.Combine(TempDir, "bin\\debug", "ddd.txt")).Close();
 
-            var files = FilesFetcher.FetchAllFiles(TempDir, new[] { ".xml" }, new[] { "bin\\" }).ToArray();
+            var files = FilesFetcher.FetchAllFiles(TempDir, new[] { ".xml" }, new[] { "BIN\\" }).ToArray();
             Assert.That(files.Length, Is.EqualTo(2));
             CollectionAssert.AreEquivalent(files.Select(u => u.Name), new[] { "AAA.cs", "ddd.txt" });
 
-            files = FilesFetcher.FetchAllFiles(TempDir, new[] { ".xml" }, new[] { "bin\\" }, "*.cs").ToArray();
+            files = FilesFetcher.FetchAllFiles(TempDir, new[] { ".xml" }, new[] { "BIN\\" }, "*.cs").ToArray();
             Assert.That(files.Length, Is.EqualTo(1));
             CollectionAssert.AreEquivalent(files.Select(u => u.Name), new[] { "AAA.cs" });
         }
