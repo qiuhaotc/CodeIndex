f8e5df85577d4ed6b7e6bcc650922ec1e6b3a689
QH-PC\qiuhao <qiu66hao@163.com>
2020-03-27 11:57:30 +0800

    modfiy file content helper

--- src/CodeIndex.Files/FilesContentHelper.cs
+++ src/CodeIndex.Files/FilesContentHelper.cs
@@ -7,42 +7,10 @@ namespace CodeIndex.Files
     {
         public static string ReadAllText(string fullName)
         {
-            byte[] contents;
+            using var fileStream = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true);
 
-            using (var fileStream = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                var fsLen = (int)fileStream.Length;
-                contents = new byte[fsLen];
-                fileStream.Read(contents, 0, contents.Length);
-            }
-
-            var bom = new byte[4];
-            for (int index = 0; index < contents.Length && index < 4; index++)
-            {
-                bom[index] = contents[index];
-            }
-
-            var encoding = GetEncoding(bom);
-
-            return encoding.GetString(contents);
-        }
-
-        /// <summary>
-        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
-        /// Defaults to ASCII when detection of the text file's endianness fails.
-        /// </summary>
-        /// <param name="filename">The text file to analyze.</param>
-        /// <returns>The detected encoding.</returns>
-        static Encoding GetEncoding(byte[] bom)
-        {
-            // Analyze the BOM
-            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
-            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
-            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
-            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
-            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
-
-            return Encoding.Default;
+            return streamReader.ReadToEnd();
         }
     }
 }
---
+++ src/CodeIndex.Test/Files/FilesContentHelperTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CodeIndex.Files;
+using NUnit.Framework;
+
+namespace CodeIndex.Test
+{
+    public class FilesContentHelperTest : BaseTest
+    {
+        [Test]
+        public void TestGetContent_ReadContentUsedByAnotherProcess()
+        {
+            using var stream1 = File.Create(Path.Combine(TempDir, "AAA.cs"));
+            stream1.Write(Encoding.UTF8.GetBytes("这是一个例句"));
+            stream1.Close();
+
+            using var stream2 = new FileStream(Path.Combine(TempDir, "AAA.cs"), FileMode.Open, FileAccess.Write);
+
+            Assert.DoesNotThrow(() =>
+            {
+                var content = FilesContentHelper.ReadAllText(Path.Combine(TempDir, "AAA.cs"));
+                Assert.AreEqual("这是一个例句", content);
+            }, "Can read file content used by another process");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EncodingTestCases))]
+        public void TestGetContent_EncodingCorrect((Encoding Encoding, string Name) encodingWithName)
+        {
+            var filePath = Path.Combine(TempDir, "AAA.cs");
+            File.WriteAllText(filePath, "这是一个例句", encodingWithName.Encoding);
+
+            var content = FilesContentHelper.ReadAllText(Path.Combine(TempDir, "AAA.cs"));
+            Assert.AreEqual("这是一个例句", content, $"Test Under {encodingWithName.Name}");
+        }
+
+        static IEnumerable<(Encoding Encoding, string Name)> EncodingTestCases()
+        {
+            yield return (Encoding.UTF8, "UTF-8");
+            yield return (Encoding.Unicode, "UTF-16LE");
+            yield return (Encoding.BigEndianUnicode, "UTF-16BE");
+            yield return (Encoding.UTF32, "UTF-32");
+        }
+    }
+}
