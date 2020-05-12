b2c048cfd5e7b37fc48afc774eac80326c52cbdf
QH-PC-NEW\qiuhao <qiu66hao@163.com>
2020-05-12 22:50:11 +0800

    should be linked file rather than add new file

--- src/CodeIndex.VisualStudioExtension/Models/CodeContentProcessing.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodeIndex.IndexBuilder
-{
-    public static class CodeContentProcessing
-    {
-        public const string HighLightPrefix = "0ffc7664bb0";
-        public const string HighLightSuffix = "b17f5526cc3";
-    }
-}
