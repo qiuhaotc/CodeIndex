51bc8d45e6e9f6a68341683a487fe980610afbff
QH-PC-NEW\qiuhao <qiu66hao@163.com>
2020-12-08 20:51:23 +0800

    Diff Match Patch

---
+++ src/CodeIndex.Test/Common/Diff_Match_PatchTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CodeIndex.Common;
+using NUnit.Framework;
+
+namespace CodeIndex.Test
+{
+    public class Diff_Match_PatchTest
+    {
+        [Test]
+        public void TestDiff()
+        {
+            var content1 = "Today is a good day" + Environment.NewLine + "WooHoo";
+            var content2 = "Tomorrow is not, what a day" + Environment.NewLine + "WOW";
+
+            var dmp = new Diff_Match_Patch();
+            var differents = dmp.Diff_Main(content1, content2);
+            dmp.Diff_CleanupSemantic(differents);
+            Assert.AreEqual(6, differents.Count);
+        }
+    }
+}
