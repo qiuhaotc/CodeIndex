0265691db65c83de8cccadb4fb85fec5ade7cb84
Qiu Hao <qiu66hao@163.com>
2025-09-27 19:55:50 +0800

    upgrade version

--- src/CodeIndex.VisualStudioExtension/Models/CodeIndexSearchViewModel.cs
+++ src/CodeIndex.VisualStudioExtension/Models/CodeIndexSearchViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows.Input;
 using CodeIndex.VisualStudioExtension.Models;
 
